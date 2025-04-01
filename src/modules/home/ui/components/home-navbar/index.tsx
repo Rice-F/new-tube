f2ea5d76b67d3fd7c562404fa05bd4e2801a6c4a
19951680900 <2604211051@qq.com>
2025-04-01 15:44:00 +0800

    add search input

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image"
 
 import { SidebarTrigger } from "@/components/ui/sidebar"
 
+import { SearchInput } from "./search-input"
+
 export const HomeNavbar = () => {
   return(
     <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
@@ -12,13 +14,20 @@ export const HomeNavbar = () => {
           <SidebarTrigger/>
           <Link href="/">
             <div className="p-4 flex items-center gap-1">
-              <Image src="/Logo.svg" alt="Logo" height={32} width={32}/>
+              <Image src="/Logo.svg" alt="Logo" height={32} width={32}/> {/* 必须使用宽高属性 */}
               <p className="text-xl font-semibold tracking-tight">Close Sea</p>
             </div>
           </Link>
         </div>
 
         {/* Search Bar */}
+        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+          <SearchInput/>
+        </div>
+
+        <div className="flex-shrink-0 items-center flex gap-4">
+          <AuthButton/>
+        </div>
       </div>
     </nav>
   )
