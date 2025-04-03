0cb3d72e41f4e7969be9600ef0b760a7742adbc9
19951680900 <2604211051@qq.com>
2025-04-03 17:28:56 +0800

    use clerk add sign signup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import {Inter} from "next/font/google";
+import {ClerkProvider} from "@clerk/nextjs";
+
 import "./globals.css";
 
 // const geistSans = Geist({
@@ -26,13 +28,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      {/* className={`${geistSans.variable} ${geistMono.variable} antialiased`} */}
-      <body
-        className={inter.className}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        {/* className={`${geistSans.variable} ${geistMono.variable} antialiased`} */}
+        <body
+          className={inter.className}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
