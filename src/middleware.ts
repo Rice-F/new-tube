7404705149f1c01426025ceec4a8b9ef4b507d76
19951680900 <2604211051@qq.com>
2025-04-05 16:29:37 +0800

    protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProjectedRoute = createRouteMatcher([
+  "/protected(.*)", // 该路由下的所有路由都需要保护
+]);
+
+export default  clerkMiddleware(async (auth, req) => {
+  if(isProjectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
