9b67a7a685430a08cb8aa643cc1f090430256bc6
19951680900 <2604211051@qq.com>
2025-04-12 18:42:32 +0800

    添加api路由并进行事件监听成功响应webhook，变更数据库

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,87 +1,57 @@
-import { Webhook } from 'svix'
-import {eq} from 'drizzle-orm'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import {db} from '@/db'
 import {users} from '@/db/schema'
+import {eq} from 'drizzle-orm'
 
 export async function POST(req: Request) {
-  const SIGNING_SECRET = process.env.CLERK_SIGNING_SECRET
-
-  if (!SIGNING_SECRET) {
-    throw new Error('Error: Please add CLERK_SIGNING_SECRET from Clerk Dashboard to .env or .env')
-  }
-
-  // Create new Svix instance with secret
-  const wh = new Webhook(SIGNING_SECRET)
-
-  // Get headers
-  const headerPayload = await headers()
-  const svix_id = headerPayload.get('svix-id')
-  const svix_timestamp = headerPayload.get('svix-timestamp')
-  const svix_signature = headerPayload.get('svix-signature')
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error: Missing Svix headers', {
-      status: 400,
-    })
-  }
-
-  // Get body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
-
-  let evt: WebhookEvent
-
-  // Verify payload with headers
   try {
-    evt = wh.verify(body, {
-      'svix-id': svix_id,
-      'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature,
-    }) as WebhookEvent
-  } catch (err) {
-    console.error('Error: Could not verify webhook:', err)
-    return new Response('Error: Verification error', {
-      status: 400,
-    })
-  }
+    console.log('请求', req)
+    const evt = await verifyWebhook(req)
 
-  // Do something with payload
-  // For this guide, log payload to console
-  const eventType = evt.type
+    const eventType = evt.type
 
-  if(eventType === 'user.created') {
-    const { data } = evt
-
-    await db.insert(users).values({
-      clerkId: data.id,
-      name: `${data.first_name} ${data.last_name}`,
-      imageUrl: data.image_url,
-    })
-  }
-
-  if(eventType === 'user.deleted') {
-    const { data } = evt
-
-    if(!data.id) {
-      return new Response('Missing user id', {
-        status: 400,
+    // user.created
+    if(eventType === 'user.created') {
+      const { data } = evt
+  
+      await db.insert(users).values({
+        clerkId: data.id,
+        name: `${data.first_name} ${data.last_name}`,
+        imageUrl: data.image_url,
       })
     }
 
-    await db.delete(users).where(eq(users.clerkId, data.id))
+    // user.deleted
+    if(eventType === 'user.deleted') {
+      const { data } = evt
+  
+      if(!data.id) {
+        return new Response('Missing user id', {
+          status: 400,
+        })
+      }
+  
+      await db
+        .delete(users)
+        .where(eq(users.clerkId, data.id))
+    }
+
+    // user.updated
+    if(eventType === 'user.updated') {
+      const { data } = evt
+  
+      await db
+        .update(users)
+        .set({
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url
+        })
+        .where(eq(users.clerkId, data.id))
+    }
+
+    return new Response('Webhook received', { status: 200 })
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error verifying webhook', { status: 400 })
   }
-
-  if(eventType === 'user.updated') {
-    const { data } = evt
-
-    await db.update(users).set({
-      name: `${data.first_name} ${data.last_name}`,
-      imageUrl: data.image_url,
-    }).where(eq(users.clerkId, data.id))
-  }
-
-  return new Response('Webhook received', { status: 200 })
 }
