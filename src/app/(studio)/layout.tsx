01c86a3696eb3b7207d47fa925fea661a09d0d6a
19951680900 <2604211051@qq.com>
2025-05-09 19:23:55 +0800

    add video schemas && relations

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -1,6 +1,6 @@
 import { StudioLayout } from "@/modules/studio/ui/layouts/studio-layout";
 interface LayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode
 };
 
 const Layout = ({children}:LayoutProps) => {
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import {relations} from "drizzle-orm";
 import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -9,6 +10,10 @@ export const users = pgTable("users", {
   updateAt: timestamp("update_at").defaultNow().notNull(),
 }, t=> [uniqueIndex("clerk_id_idx").on(t.clerkId)]); // 唯一索引，搜索优化
 
+export const usersRelations = relations(users, ({ many }) => ({
+  videos: many(videos), // 一对多关系，用户可以有多个视频，或者为空
+}));
+
 export const categories = pgTable("categories", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull().unique(),
@@ -16,3 +21,32 @@ export const categories = pgTable("categories", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updateAt: timestamp("update_at").defaultNow().notNull(),
 }, t => [uniqueIndex("name_idx").on(t.name)]);
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  videos: many(videos), // 一对多关系，一个分类可以有多个视频，或者为空
+}));  
+
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: text("title").notNull(),
+  description: text("description"),
+  userId: uuid("user_id").references(() => users.id, { // 外键 - 关联到users表的id字段
+    onDelete: "cascade", // 级联删除，如果用户被删除，则删除该用户的所有视频
+  }).notNull(),
+  categoryId: uuid("category_id").references(() => categories.id, {
+    onDelete: "set null", // 级联删除，如果分类被删除，则将视频的分类设置为null，但不删除视频
+  }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updateAt: timestamp("update_at").defaultNow().notNull(),
+})
+
+export const videoRelations = relations(videos, ({ one }) => ({
+  user: one(users, {
+    fields: [videos.userId],
+    references: [users.id],
+  }),
+  category: one(categories, {
+    fields: [videos.categoryId],
+    references: [categories.id],
+  }),
+}))
