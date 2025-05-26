f08138f7b830c1209912d2c09a5cdbd36d80f3b4
19951680900 <2604211051@qq.com>
2025-05-26 18:08:25 +0800

    add upload modal dialog

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import {toast} from 'sonner'
-import {Button} from '@/components/ui/button';
 import {Loader2Icon, PlusIcon} from 'lucide-react';
 
 import {trpc} from "@/trpc/client"
+import {Button} from '@/components/ui/button';
+import { ResponsiveModal } from '@/components/responsive-modal'
 
 export const StudioUploadModal = () => {
   const utils = trpc.useUtils()
 
+  // create是一个mutation对象
+  // useMutation 处理数据写操作（create、update、delete）等的Hook
   const create = trpc.videos.create.useMutation({
     onSuccess: () => {
       toast.success('Video created')
@@ -20,13 +23,22 @@ export const StudioUploadModal = () => {
   });
 
   return (
-    <Button
-      variant="secondary"
-      onClick={() => create.mutate()} // 触发mutation
-      disabled={create.isPending}
-    >
-      {create.isPending ? <Loader2Icon className='animate-spin' /> : <PlusIcon />}
-      Create
-    </Button>
+    <>
+      <ResponsiveModal
+        title="Upload a video"
+        open={!!create.data} // create.data 是 mutation 返回的数据（比如服务器创建后返回的新视频）
+        onOpenChange={() => {create.reset()}} // 关闭时重置mutation状态
+      >
+        <p>this is upload</p>
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()} // 触发mutation，提交请求
+        disabled={create.isPending}
+      >
+        {create.isPending ? <Loader2Icon className='animate-spin' /> : <PlusIcon />}
+        Create
+      </Button>
+    </>
   )
 }
